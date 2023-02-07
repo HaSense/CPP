8f20f0f6c1e4d73b85416331fc12d5791b69ba20
Virtual Sense <virtual_sense@naver.com>
2023-02-07 11:06:56 +0900

    Update oop_operator02.cpp

--- Core/class/oop_operator02.cpp
+++ Core/class/oop_operator02.cpp
@@ -16,9 +16,9 @@ int main() {
     Complex c1(1, 2), c2(3, 4);
     Complex c3 = c1 + c2;
 
-    std::cout << "c1: " << c1.real << " + " << c1.imag << "i\n";
-    std::cout << "c2: " << c2.real << " + " << c2.imag << "i\n";
-    std::cout << "c3: " << c3.real << " + " << c3.imag << "i\n";
+    std::cout << "c1: " << c1.real << " + " << c1.imag << "i" << std::endl;
+    std::cout << "c2: " << c2.real << " + " << c2.imag << "i" << std::endl;
+    std::cout << "c3: " << c3.real << " + " << c3.imag << "i" << std::endl;
 
     return 0;
 }
