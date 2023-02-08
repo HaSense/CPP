bf94743705e15a01c353f6d7a193d7dfeaac36e9
Virtual Sense <virtual_sense@naver.com>
2023-02-08 12:58:06 +0900

    Update mat_array02.cpp

--- opencv/2D/mat_array02.cpp
+++ opencv/2D/mat_array02.cpp
@@ -6,6 +6,7 @@ using namespace cv;
 
 int main()
 {
+   //Stack에 배열선언
    int arr[3][3];
    
    int cnt = 1;
@@ -20,7 +21,7 @@ int main()
     }
     cout<<endl;
    }
-
+   //동적할당 Heap
    int **arr2 = new int*[3]; //*[row] 동적할당 선언
    for(int i=0; i<3; i++) //각각의 행에 길이가 4인 배열을 할당
    arr2[i] = new int[3];
@@ -37,7 +38,9 @@ int main()
     }
     cout<<endl;
    } 
-
+   delete[] arr2; //heap에 객체 메모리 해제
+   
+   //Mat 표현
    Mat m2 = (Mat_<int>(3,3) << 1, 4, 7, 2, 5, 8, 3, 6, 9);
    cout << "m2 : " << endl << m2 << endl;; 
 
