4be023aafc18f857c2661e0c2951e6ff652599ca
Virtual Sense <virtual_sense@naver.com>
2023-02-21 15:15:34 +0900

    미니 주소록 스켈레톤 코드

--- Core/quiz/test.cpp
+++ Core/quiz/test.cpp
@@ -1,4 +1,50 @@
-int select = -1;
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Person
+{
+public:
+    Person():no(0), name(NULL), phoneNumber(NULL){
+    }
+    Person(int no, string name, string phoneNumber)
+        :no(no), name(name), phoneNumber(phoneNumber) {
+
+    }
+    //Getter
+    int No(){
+        return no;
+    }
+    string Name(){
+        return name;
+    }
+    string PhoneNumber(){
+        return phoneNumber;
+    }
+    //Setter
+    void setNo(int no_){
+        no = no_;
+    }
+    void setName(string name_){
+        name = name_;
+    }
+    void setPhoneNumber(string phoneNumber_){
+        phoneNumber = phoneNumber_;
+    }
+private:
+    int no;
+    string name;
+    string phoneNumber;
+};
+
+int main()
+{
+    vector<Person> addressbook;
+
+    int select = -1;
     do
     {
         cout << "---------------------------------------------------------------- -" << endl;
@@ -12,9 +58,24 @@ int select = -1;
     
         switch (select)
         {
+            case 1:
+                //주소록 조회 코드를 작성합니다.
+                break;
+            case 2:
+                //주소록 추가 코드를 작성합니다.
+                break;
             case 5:
                 exit(1); 
                 break;
         }
     
     } while (select != 0);
+
+}
+
+
+
+
+
+
+
