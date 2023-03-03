00cf9fbe687238404a0d69461b37922606d7b8d4
Virtual Sense <virtual_sense@naver.com>
2023-03-03 12:39:17 +0900

    빌더패턴

--- Pattern/Creational/Builder/builder_1/builder_1.cpp
+++ Pattern/Creational/Builder/builder_1/builder_1.cpp
@@ -1,76 +1,144 @@
 #include <iostream>
+#include <string>
 #include <memory>
 
-class Car {
-public:
-    Car() {}
-    virtual ~Car() {}
+using namespace std;
 
-    virtual std::string get_brand() const = 0;
-    virtual std::string get_model() const = 0;
-    virtual int get_year() const = 0;
+// Car 클래스
+class Car {
+protected:
+    string m_brand;     // 브랜드
+    string m_model;     // 모델명
+    int m_year;         // 연식
+    string m_engine;    // 엔진 타입
+
+public:
+    void set_brand(const string& brand) {
+        m_brand = brand;
+    }
+
+    void set_model(const string& model) {
+        m_model = model;
+    }
+
+    void set_year(int year) {
+        m_year = year;
+    }
+
+    void set_engine(const string& engine) {
+        m_engine = engine;
+    }
+
+    string to_string() const {
+        return m_brand + " " + m_model + " (" + to_string(m_year) + "), " + m_engine;
+    }
 };
 
-class CarBuilder {
+// Builder 인터페이스
+class Builder {
+protected:
+    unique_ptr<Car> m_car;
+
 public:
-    CarBuilder() {}
-    virtual ~CarBuilder() {}
+    Car* get_car() {
+        return m_car.release();
+    }
+
+    void create_new_car() {
+        m_car = make_unique<Car>();
+    }
 
     virtual void set_brand() = 0;
     virtual void set_model() = 0;
     virtual void set_year() = 0;
-    virtual std::unique_ptr<Car> get_car() = 0;
+    virtual void set_engine() = 0;
 };
 
-class SportsCarBuilder : public CarBuilder {
-private:
-    std::unique_ptr<Car> m_car;
-
+// SportsCarBuilder 클래스
+class SportsCarBuilder : public Builder {
 public:
-    SportsCarBuilder() {}
-    virtual ~SportsCarBuilder() {}
-
     virtual void set_brand() override {
-        m_car->m_brand = "Ferrari";
+        m_car->set_brand("Ferrari");
     }
 
     virtual void set_model() override {
-        m_car->m_model = "458 Spider";
+        m_car->set_model("488 Spider");
     }
 
     virtual void set_year() override {
-        m_car->m_year = 2012;
+        m_car->set_year(2022);
     }
 
-    virtual std::unique_ptr<Car> get_car() override {
-        return std::move(m_car);
+    virtual void set_engine() override {
+        m_car->set_engine("3.9L V8 Twin-Turbo");
     }
 };
 
-class CarManufacturingDirector {
+// SUVBuilder 클래스
+class SUVBuilder : public Builder {
 public:
-    CarManufacturingDirector() {}
-    virtual ~CarManufacturingDirector() {}
+    virtual void set_brand() override {
+        m_car->set_brand("BMW");
+    }
 
-    std::unique_ptr<Car> make_car(CarBuilder* builder) {
-        builder->m_car = std::make_unique<Car>();
-        builder->set_brand();
-        builder->set_model();
-        builder->set_year();
-        return builder->get_car();
+    virtual void set_model() override {
+        m_car->set_model("X7");
+    }
+
+    virtual void set_year() override {
+        m_car->set_year(2022);
+    }
+
+    virtual void set_engine() override {
+        m_car->set_engine("4.4L V8 Twin-Turbo");
     }
 };
 
+// Director 클래스
+class Director {
+private:
+    Builder* m_builder;
+
+public:
+    void set_builder(Builder* builder) {
+        m_builder = builder;
+    }
+
+    Car* get_car() {
+        return m_builder->get_car();
+    }
+
+    void construct_car() {
+        m_builder->create_new_car();
+        m_builder->set_brand();
+        m_builder->set_model();
+        m_builder->set_year();
+        m_builder->set_engine();
+    }
+};
+
+// 메인 함수
 int main() {
-    CarManufacturingDirector car_manufacturing_director;
+    Director director;
+    SportsCarBuilder sports_car_builder;
+    SUVBuilder suv_builder;
 
-    std::unique_ptr<CarBuilder> sports_car_builder = std::make_unique<SportsCarBuilder>();
-    std::unique_ptr<Car> sports_car = car_manufacturing_director.make_car(std::move(sports_car_builder));
+    // 스포츠카 만들기
+    director.set_builder(&sports_car_builder);
+    director.construct_car();
+    Car* sports_car = director.get_car();
 
-    std::cout << "Sports car:" << std::endl;
-    std::cout << "Brand: " << sports_car->get_brand() << std::endl;
-    std::cout << "Model: " << sports_car->get_model() << std::endl;
-    std::cout << "Year: " << sports_car->get_year() << std::endl;
+    // SUV 만들기
+    director.set_builder(&suv_builder);
+    director.construct_car();
+    Car* suv = director.get_car();
+
+    // 결과 출력
+    cout << "Sports car: " << sports_car->to_string() << endl;
+    cout << "SUV: " << suv->to_string() << endl;
+
+    delete sports_car;
+    delete suv;
 
     return 0;
 }
