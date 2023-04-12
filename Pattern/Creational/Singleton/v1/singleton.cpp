ad5c4f4152ca97f716d3b745c832488c8f3f379b
Virtual Sense <virtual_sense@naver.com>
2023-04-12 14:35:41 +0900

    Update singleton.cpp

--- Pattern/Creational/Singleton/v1/singleton.cpp
+++ Pattern/Creational/Singleton/v1/singleton.cpp
@@ -1,31 +1,47 @@
 #include <iostream>
 
-class Singleton {
+class Singleton
+{
 private:
-    // 생성자, 복사 생성자, 대입 연산자 모두 private으로 선언하여 외부에서의 객체 생성을 방지
-    Singleton() {}
+    Singleton(){}
     Singleton(const Singleton&);
     Singleton& operator=(const Singleton&);
 
 public:
-    // 유일한 인스턴스를 반환하는 정적 메소드 GetInstance()
+    // 유일한 Instance를 반환하는 static method
     static Singleton& GetInstance() {
-        static Singleton instance; // 정적 지역 변수로 인스턴스를 유지
+        static Singleton instance;
         return instance;
     }
-
-    // 기능을 제공하는 메소드
     void DoSomething() {
         std::cout << "Hello, Singleton!" << std::endl;
+    }
+    std::size_t getHashCoe() const {
+        return std::hash<const Singleton*>()(this);
     }
 };
 
 int main() {
+
     Singleton& s1 = Singleton::GetInstance();
     Singleton& s2 = Singleton::GetInstance();
 
     s1.DoSomething();
     s2.DoSomething();
 
+    std::cout << s1.getHashCoe() << std::endl;
+    std::cout << s2.getHashCoe() << std::endl;
+
     return 0;
 }
+
+/*
+
+Hello, Singleton!
+Hello, Singleton!
+94879569248562
+94879569248562
+
+*/
+
+
