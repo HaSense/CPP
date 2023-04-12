f4cff3e14b403cb8044cafd72225c997bab5645d
Virtual Sense <virtual_sense@naver.com>
2023-04-12 10:39:05 +0900

    Create 05.stl_add.cpp

---
+++ Core/template/05.stl_add.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+int main() {
+  std::vector<int> numbers {1, 2, 3, 4, 5};
+  int sum = std::accumulate(numbers.begin(), numbers.end(), 0);
+  std::cout << "The sum is: " << sum << std::endl;
+  return 0;
+}
