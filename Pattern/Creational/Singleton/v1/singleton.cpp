4e7581c09cd56f6fa124d404522c38666eef2594
Virtual Sense <virtual_sense@naver.com>
2023-04-12 14:43:41 +0900

    Update singleton.cpp

--- Pattern/Creational/Singleton/v1/singleton.cpp
+++ Pattern/Creational/Singleton/v1/singleton.cpp
@@ -21,27 +21,39 @@ public:
     }
 };
 
+class Cat
+{
+public:
+    std::size_t getHashCoe() const {
+        return std::hash<const Cat*>()(this);
+    }
+};
+
 int main() {
 
     Singleton& s1 = Singleton::GetInstance();
     Singleton& s2 = Singleton::GetInstance();
+    Singleton& s3 = Singleton::GetInstance();
 
-    s1.DoSomething();
-    s2.DoSomething();
+    std::cout << "s1 : " << s1.getHashCoe() << std::endl;
+    std::cout << "s2 : " << s2.getHashCoe() << std::endl;
+    std::cout << "s3 : " << s3.getHashCoe() << std::endl;
 
-    std::cout << s1.getHashCoe() << std::endl;
-    std::cout << s2.getHashCoe() << std::endl;
+    Cat cat1, cat2, cat3;
+    std::cout << "cat1 : " << cat1.getHashCoe() << std::endl;
+    std::cout << "cat2 : " << cat2.getHashCoe() << std::endl;
+    std::cout << "cat3 : " << cat3.getHashCoe() << std::endl;
 
     return 0;
 }
 
 /*
-
-Hello, Singleton!
-Hello, Singleton!
-94879569248562
-94879569248562
-
+s1 : 94837060084018
+s2 : 94837060084018
+s3 : 94837060084018
+cat1 : 140723780945005
+cat2 : 140723780945006
+cat3 : 140723780945007
 */
 
 
